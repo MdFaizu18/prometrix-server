9fe5a0e5f83957227a3c6d16d769d52e243181b4
Md Faizu <mohammed17092002@gmail.com>
2026-02-27 15:14:12 +0530

    Made changes according to vercel 2

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 // ─────────────────────────────────────────────
 //  SECURITY MIDDLEWARE
 // ─────────────────────────────────────────────
-
+app.set("trust proxy", 1);
 app.use(helmet());
 
 app.use(
